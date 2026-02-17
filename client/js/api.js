0f181258395a4b8bb85a7ed94b7f40540cdc5422
Harry Chopra <22281143+harrychopra@users.noreply.github.com>
2026-02-17 23:49:00 +0000

    bugfix: requestAPI - fail fast on !res.ok

--- client/js/api.js
+++ client/js/api.js
@@ -5,22 +5,25 @@ export async function requestAPI({ method, url, payload, ctx }) {
 
   try {
     if (payload !== undefined) {
-      opts['body'] = JSON.stringify(payload);
+      opts.body = JSON.stringify(payload);
     }
 
     const res = await fetch(url, opts);
 
-    if (!res.ok) {
-      logError(ctx, `${method} ${url} failed with status ${res.status}`);
-      showToast(`Could not ${ctx}`, true);
-    }
-
-    const resContentType = res.headers.get('Content-Type');
-
-    // Where we aren't returning any data eg DELETE a note
-    const result = resContentType?.includes('application/json')
+    // requests like DELETE often wont return data
+    const contentType = res.headers.get('Content-Type');
+    const result = contentType?.includes('application/json')
       ? await res.json()
       : null;
+
+    if (!res.ok) {
+      const message =
+        `${ctx} ${method} ${url} failed with status ${res.status}`;
+      logError(message, result);
+      showToast(`Could not ${ctx}`, true);
+
+      return [null, new Error()];
+    }
 
     return [result, null];
   } catch (err) {
